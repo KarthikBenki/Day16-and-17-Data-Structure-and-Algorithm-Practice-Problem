df6522b08e65579043d85108849b036400c6343d
karthikbenki <karthikmc007@gmail.com>
2022-03-04 19:52:01 +0530

    Added: BubbleSort logic to sort string array

--- src/com/blz/datastructure/BubbleSort.java
+++ src/com/blz/datastructure/BubbleSort.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 
 /*
  //* @Purpose: BubbleSort Sort
-    a. Desc -> Reads in strings and prints them in sorted order using insertion sort.
+    a. Desc -> Reads in Integer and prints them in sorted order using insertion sort.
     b. I/P -> read in the list words
-    c. Logic -> Use Insertion Sort to sort the words in the String array
+    c. Logic -> Use Insertion Sort to sort the words in the Integer array
     d. O/P -> Print the Sorted List
  //* @file : InsertionSort.java
  //* @author : Karthik M C
@@ -17,12 +17,12 @@ public class BubbleSort {
 
     /*
      * @purpose: Ability to BubbleSort sort
-     * @param: Takes String array as input
+     * @param: Takes integer array as input
      * @function : sorts the given array in ascending order and stores them in their
      * indecencies
      * @return: No return value.
      */
-    static public void bubbleSort(String[] array) {
+    static public void bubbleSort(Integer[] array) {
         for (int i = 1; i < array.length; i++) {
             int size = array.length;
 
@@ -38,7 +38,7 @@ public class BubbleSort {
 
                         // swapping occurs if elements
                         // are not in the intended order
-                        String temp = array[k];
+                        Integer temp = array[k];
                         array[k] = array[k + 1];
                         array[k + 1] = temp;
                     }
@@ -46,38 +46,38 @@ public class BubbleSort {
     }
 
     /*
-     * @purpose: Ability to print String array
-     * @param: Takes String array as input
-     * @function : print the given string array
+     * @purpose: Ability to print integer array
+     * @param: Takes integer array as input
+     * @function : print the given integer array
      * @return: No return value.
      */
-    static public void printString(String[] strings) {
-        for (int i = 0; i < strings.length; i++) {
-            System.out.println("string at index " + i + " is " + strings[i]);
+    static public void printArray(Integer[] integers) {
+        for (int i = 0; i < integers.length; i++) {
+            System.out.println("Integer at index " + i + " is " + integers[i]);
         }
     }
 
     /*
-     * @purpose: Ability to scan String array
-     * @param: Takes String array as input
-     * @function : scans the data from user and stores in to string array
+     * @purpose: Ability to scan integer array
+     * @param: Takes integer array as input
+     * @function : scans the data from user and stores in to integer array
      * @return: No return value.
      */
-    static public void scanString(String[] strings) {
-        for (int i = 0; i < strings.length; i++) {
-            System.out.printf("Enter the string at index :%d\n", i);
-            strings[i] = scanner.nextLine();
+    static public void scanArray(Integer[] integers) {
+        for (int i = 0; i < integers.length; i++) {
+            System.out.printf("Enter the integer at index :%d\n", i);
+            integers[i] = scanner.nextInt();
         }
     }
 
     public static void main(String[] args) {
-        String[] strings = new String[5];
-        scanString(strings);
+        Integer[] integers = new Integer[5];
+        scanArray(integers);
         System.out.println("***********************Before Sort*************************");
-        printString(strings);
-        bubbleSort(strings);
+        printArray(integers);
+        bubbleSort(integers);
         System.out.println("***********************After Sort*************************");
-        printString(strings);
+        printArray(integers);
 
     }
 }
